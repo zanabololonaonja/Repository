b52ff1d2e5ea4e0ffc8f0bf803f5690a3de3453a
ONJA <zanabololonaonja@gmail.com>
2024-08-16 10:18:29 +0200

    backend

--- Back/config/db.js
+++ Back/config/db.js
@@ -1,18 +1,25 @@
+require('dotenv').config(); // Charger les variables d'environnement à partir du fichier .env
+
 const { Pool } = require('pg');
 
+// Construire la chaîne de connexion à partir de la variable d'environnement
+const connectionString = process.env.POSTGRES_URL;
+
+console.log('Connecting to PostgreSQL with URL:', connectionString);
+
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: 'onja',
-  database: 'mlr1',
-  port: 5432,
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false // Optionnel, en fonction de votre configuration SSL
+  }
 });
 
-pool.connect((err) => {
+pool.connect((err, client, release) => {
   if (err) {
     console.error('Error connecting to PostgreSQL', err.stack);
   } else {
     console.log('Connected to PostgreSQL successfully');
+    release();
   }
 });
 
