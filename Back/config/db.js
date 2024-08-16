1d48c71d0a5462460200404a91e1e87e165ee333
ONJA <zanabololonaonja@gmail.com>
2024-08-16 10:34:40 +0200

    vercel

--- Back/config/db.js
+++ Back/config/db.js
@@ -24,3 +24,29 @@ pool.connect((err, client, release) => {
 });
 
 module.exports = pool;
+
+
+
+
+
+
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: 'postgres',
+//   password: 'onja',
+//   database: 'mlr1',
+//   port: 5432,
+// });
+
+// pool.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to PostgreSQL', err.stack);
+//   } else {
+//     console.log('Connected to PostgreSQL successfully');
+//   }
+// });
+
+// module.exports = pool;
+
